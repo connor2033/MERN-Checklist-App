85e55574e0846b5cdd864b81b94b3bd1c28d3392
Connor Haines <connor2033@gmail.com>
2022-03-30 21:22:35 -0400

    POST new Checklists and redirects to /preview/{id}

--- frontend/src/screens/NewListPage/NewListPage.js
+++ frontend/src/screens/NewListPage/NewListPage.js
@@ -8,57 +8,127 @@ import {
 } from "react-bootstrap";
 import "./NewListPage.css";
 import { useState } from "react";
+import axios from "axios";
 
 function NewListPage() {
-  const [inputList, setInputList] = useState([
-    { itemType: "checklistItem", itemName: "", isChecked: false },
-  ]);
+  const [checklist, setChecklist] = useState({
+    title: "",
+    details: "",
+    listItems: [{ itemType: "checklistItem", itemName: "", isChecked: false }],
+  });
+
+  const postChecklist = async () => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/checklist/create",
+      checklist,
+      config
+    );
+
+    console.log(data);
+    window.location.href = "/preview/" + data._id;
+  };
 
   // handle input change
-  const handleInputChange = (e, index) => {
+  const handleTopInputChange = (e, field) => {
+    const { value } = e.target;
+    const newChecklist = {
+      title: checklist.title,
+      details: checklist.details,
+      listItems: [...checklist.listItems],
+    };
+
+    if (field === "title") {
+      newChecklist.title = value;
+    } else if (field === "details") {
+      newChecklist.details = value;
+    }
+
+    setChecklist(newChecklist);
+  };
+
+  // handle list input change
+  const handleListInputChange = (e, index) => {
     const { name, value } = e.target;
-    const list = [...inputList];
+    const list = [...checklist.listItems];
     list[index][name] = value;
-    setInputList(list);
+
+    const newChecklist = {
+      title: checklist.title,
+      details: checklist.details,
+      listItems: list,
+    };
+
+    setChecklist(newChecklist);
   };
 
   // handle click event of the Remove button
   const handleRemoveClick = (index) => {
-    if (inputList.length > 1) {
-      const list = [...inputList];
+    if (checklist.listItems.length > 1) {
+      const list = [...checklist.listItems];
       list.splice(index, 1);
-      setInputList(list);
+
+      const newChecklist = {
+        title: checklist.title,
+        details: checklist.details,
+        listItems: list,
+      };
+
+      setChecklist(newChecklist);
     }
   };
 
   //add Heading object to list
   //Don't allow headings next to eachother
   const handleAddHeadingClick = (index) => {
-    if (inputList[index].itemType !== "heading") {
+    if (checklist.listItems[index].itemType !== "heading") {
       if (
-        (index > 0 && inputList[index - 1].itemType !== "heading") ||
+        (index > 0 && checklist.listItems[index - 1].itemType !== "heading") ||
         index === 0
       ) {
-        const listStart = [...inputList.splice(index, inputList.length)];
-        const listEnd = [...inputList.splice(0, index)];
-        setInputList([
-          ...listEnd,
-          { itemType: "heading", itemName: "", isChecked: false },
-          ...listStart,
-        ]);
+        const listStart = [
+          ...checklist.listItems.splice(index, checklist.listItems.length),
+        ];
+        const listEnd = [...checklist.listItems.splice(0, index)];
+
+        const newChecklist = {
+          title: checklist.title,
+          details: checklist.details,
+          listItems: [
+            ...listEnd,
+            { itemType: "heading", itemName: "", isChecked: false },
+            ...listStart,
+          ],
+        };
+
+        setChecklist(newChecklist);
       }
     }
   };
 
   // handle click event of the Add button with index
   const handleAddClick = (index) => {
-    const listStart = [...inputList.splice(index + 1, inputList.length)];
-    const listEnd = [...inputList.splice(0, index + 1)];
-    setInputList([
-      ...listEnd,
-      { itemType: "checklistItem", itemName: "", isChecked: false },
-      ...listStart,
-    ]);
+    const listStart = [
+      ...checklist.listItems.splice(index + 1, checklist.listItems.length),
+    ];
+    const listEnd = [...checklist.listItems.splice(0, index + 1)];
+
+    const newChecklist = {
+      title: checklist.title,
+      details: checklist.details,
+      listItems: [
+        ...listEnd,
+        { itemType: "checklistItem", itemName: "", isChecked: false },
+        ...listStart,
+      ],
+    };
+
+    setChecklist(newChecklist);
   };
 
   // handle keyDown event for add
@@ -85,14 +155,15 @@ function NewListPage() {
               <input
                 type="text"
                 placeholder="List Name"
-                maxlength="48"
-                autocomplete="off"
+                maxLength="48"
+                autoComplete="off"
                 autoFocus="autofocus"
                 className="listNameBox"
                 id="titleBox"
                 onKeyUp={(e) => {
                   handleFocus(e, "detailBox");
                 }}
+                onChange={(e) => handleTopInputChange(e, "title")}
               ></input>
               <hr />
             </Row>
@@ -101,38 +172,42 @@ function NewListPage() {
               <input
                 type="text"
                 placeholder="Details..."
-                maxlength="240"
-                autocomplete="off"
+                maxLength="240"
+                autoComplete="off"
                 className="listDetailsBox"
                 id="detailBox"
                 onKeyUp={(e) => {
                   handleFocus(e, 0);
                 }}
+                onChange={(e) => handleTopInputChange(e, "details")}
               ></input>
             </Row>
             {/* Item Row */}
             <div className="scrollBox" id="scrollBox">
-              {inputList.map((x, i) => {
+              {checklist.listItems.map((item, index) => {
                 // HEADING
-                if (x.itemType === "heading") {
+                if (item.itemType === "heading") {
                   return (
-                    <Row style={{ marginLeft: "0px", width: "100%" }}>
+                    <Row
+                      style={{ marginLeft: "0px", width: "100%" }}
+                      key={index}
+                    >
                       <Col className="newHeadingShell">
                         <input
                           name="itemName"
-                          value={x.itemName}
-                          onChange={(e) => handleInputChange(e, i)}
-                          id={i}
+                          value={item.itemName}
+                          onChange={(e) => handleListInputChange(e, index)}
+                          id={index}
                           type="text"
                           placeholder="Add a subheading..."
-                          maxlength="48"
-                          autocomplete="off"
+                          maxLength="48"
+                          autoComplete="off"
                           className="inputBox"
                           onKeyDown={(e) => {
-                            handleAddKey(e, i);
+                            handleAddKey(e, index);
                           }}
                           onKeyUp={(e) => {
-                            handleFocus(e, i + 1);
+                            handleFocus(e, index + 1);
                           }}
                         ></input>
                         <hr style={{ width: "70%" }} />
@@ -141,7 +216,7 @@ function NewListPage() {
                         <Dropdown as={ButtonGroup} style={{ marginTop: "5px" }}>
                           <Button
                             variant="success"
-                            onClick={() => handleAddClick(i)}
+                            onClick={() => handleAddClick(index)}
                           >
                             +
                           </Button>
@@ -150,11 +225,13 @@ function NewListPage() {
 
                           <Dropdown.Menu align="end">
                             <Dropdown.Item
-                              onClick={() => handleAddHeadingClick(i)}
+                              onClick={() => handleAddHeadingClick(index)}
                             >
                               Add Subheading
                             </Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleRemoveClick(i)}>
+                            <Dropdown.Item
+                              onClick={() => handleRemoveClick(index)}
+                            >
                               Remove
                             </Dropdown.Item>
                           </Dropdown.Menu>
@@ -166,23 +243,23 @@ function NewListPage() {
 
                 // CHECKLIST ITEM
                 return (
-                  <Row style={{ marginLeft: "0px", width: "100%" }}>
+                  <Row style={{ marginLeft: "0px", width: "100%" }} key={index}>
                     <Col className="newItemshell">
                       <input
                         name="itemName"
-                        value={x.itemName}
-                        onChange={(e) => handleInputChange(e, i)}
-                        id={i}
+                        value={item.itemName}
+                        onChange={(e) => handleListInputChange(e, index)}
+                        id={index}
                         type="text"
                         placeholder="Add an item..."
-                        maxlength="90"
-                        autocomplete="off"
+                        maxLength="90"
+                        autoComplete="off"
                         className="inputBox"
                         onKeyDown={(e) => {
-                          handleAddKey(e, i);
+                          handleAddKey(e, index);
                         }}
                         onKeyUp={(e) => {
-                          handleFocus(e, i + 1);
+                          handleFocus(e, index + 1);
                         }}
                       ></input>
                     </Col>
@@ -190,7 +267,7 @@ function NewListPage() {
                       <Dropdown as={ButtonGroup} style={{ marginTop: "5px" }}>
                         <Button
                           variant="success"
-                          onClick={() => handleAddClick(i)}
+                          onClick={() => handleAddClick(index)}
                         >
                           +
                         </Button>
@@ -199,11 +276,13 @@ function NewListPage() {
 
                         <Dropdown.Menu align="end">
                           <Dropdown.Item
-                            onClick={() => handleAddHeadingClick(i)}
+                            onClick={() => handleAddHeadingClick(index)}
                           >
                             Add Subheading
                           </Dropdown.Item>
-                          <Dropdown.Item onClick={() => handleRemoveClick(i)}>
+                          <Dropdown.Item
+                            onClick={() => handleRemoveClick(index)}
+                          >
                             Remove
                           </Dropdown.Item>
                         </Dropdown.Menu>
@@ -215,13 +294,17 @@ function NewListPage() {
             </div>
             <Row style={{ display: "flex", justifyContent: "right" }}>
               {/* Temp href to /preview */}
-              <Button variant="dark" className="saveBtn" href="/preview">
+              <Button
+                variant="dark"
+                className="saveBtn"
+                onClick={() => postChecklist()}
+              >
                 Create
               </Button>
             </Row>
           </div>
           {/* \/ for testing list as JSON */}
-          <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
+          {/* <div style={{ marginTop: 20 }}>{JSON.stringify(checklist)}</div> */}
         </Container>
       </div>
     </>
