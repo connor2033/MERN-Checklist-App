df220649cc1977b13a55f1be28af7aadfa00ec9e
Connor Haines <connor2033@gmail.com>
2022-06-25 15:10:08 -0400

    Implementing deployment code

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,30 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const checklistRoutes = require("./routes/checklistRoutes");
+const path = require("path");
 
 const app = express();
 dotenv.config();
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use(express.json());
 app.use("/api/checklist", checklistRoutes);
+
+// --------Deployment--------
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// --------Deployment--------
 
 const PORT = process.env.PORT || 5000;
 
